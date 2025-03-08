ac50255befe26d15b7615e334cc566024b4b7b3b
Martin Vlk <vlkmart7@fel.cvut.cz>
2025-03-08 19:59:54 +0100

    phpstan fix - type error

--- party-portal/apps/backend/auth-service/public/index.php
+++ party-portal/apps/backend/auth-service/public/index.php
@@ -5,5 +5,9 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $environment = isset($context['APP_ENV']) && is_string($context['APP_ENV']) 
+        ? $context['APP_ENV'] 
+        : 'dev';
+    
+    return new Kernel($environment, (bool) $context['APP_DEBUG']);
 };
